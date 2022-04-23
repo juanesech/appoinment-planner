2dc1d218df6108fcab89b78f4db29465796645d2
Juanesech <juanesech@gmail.com>
2022-04-23 15:19:35 +0200

    add contacts handlers

--- pages/Contacts.js
+++ pages/Contacts.js
@@ -1,11 +1,12 @@
 import Layout from "../loyouts";
 import { useState } from "react";
+import ContactForm from "../components/ContactForm";
+import TileList from "../components/TileList";
 
 const Contacts = () => {
 
-  const [constacts, setContacts] = useState([]);
-  const addNewContact = (newContact) => {
-    const {name, phoneNumber, email} = newContact;
+  const [contacts, setContacts] = useState([]);
+  const addNewContact = ({ name, phoneNumber, email }) => {
     setContacts((prev) => {
       [
         ...prev,
@@ -18,23 +19,51 @@ const Contacts = () => {
     });
   }
 
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState();
+  const [email, setEmail] = useState('');
+
+  const checkContactDuplicity = (name) => {
+    let exists = Contacts.find((contact) => contact.name === name);
+    if (exists){
+      console.log(`Contact with ${name} already exists.`);
+      return true
+    }
+    return false
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    /*
-    Add contact info and clear data
-    if the contact name is not a duplicate
-    */
+    const { value } = e.target;
+    
+    if (!checkContactDuplicity(value.name)){
+      addNewContact(value);
+      setName('');
+      setEmail('');
+      setPhone('');
+      return
+    }
+    alert(`Contact with ${value.name} already exists.`)
   };
 
   return (
     <Layout>
       <div>
         <section>
-          <h2>Add Contact</h2> 
+          <h2>Add Contact</h2>
+          <ContactForm
+            name={name}
+            phone={phone}
+            email={email}
+            handleSubmit={handleSubmit}
+          />
         </section>
         <hr />
         <section>
           <h2>Contacts</h2>
+          <TileList
+            contactArray={contacts}
+          />
         </section>
       </div>
     </Layout>
